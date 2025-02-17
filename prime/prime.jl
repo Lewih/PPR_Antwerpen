e4800650c691aa6648d6168e485ac74ab3ec723f
Ine Arts <29948516+IneMEGAmaxi@users.noreply.github.com>
2025-02-17 15:09:45 +0100

    Update prime.jl
    added (!) to exercise comments

--- prime/prime.jl
+++ prime/prime.jl
@@ -1,6 +1,6 @@
 # Simple example of bruteforce prime numbers search
-# to run interactive session -> julia -p n -t m -i prime.jl
-# or copy paste the code in the repl
+#(!) to run interactive session -> julia -p n -t m -i prime.jl
+#(!) or copy paste the code in the repl
 
 
 # using the packages and install BenchmarkTools if not installed
@@ -35,21 +35,21 @@ function prime(n=100000)
     end
 end
 
-# run once to compile
+#(!) run once to compile
 prime(10)
 @info "Code has been executed once"
-# or use BenchmarkTools @btime prime() to benchmark
+#(!) or use BenchmarkTools @btime prime() to benchmark
 
 
-## https://www.julia-vscode.org/docs/stable/userguide/profiler/
-# Try to see in the profiling if you can see something interesting or whether the @sync, @distributed and @threads macros are obscuring everything.
-# If so, which processes are used to compute (workers)? Are you profiling a "Master" process which is not computing?
-## https://docs.julialang.org/en/v1/manual/distributed-computing/
+##(!) https://www.julia-vscode.org/docs/stable/userguide/profiler/
+#(!) Try to see in the profiling if you can see something interesting or whether the @sync, @distributed and @threads macros are obscuring everything.
+#(!) If so, which processes are used to compute (workers)? Are you profiling a "Master" process which is not computing?
+##(!) https://docs.julialang.org/en/v1/manual/distributed-computing/
 # stacktrace profiling
 @profview prime()
 # allocation profiling
 @profview_allocs prime()
-# try to think about the difference between workers and master process
+#(!) try to think about the difference between workers and master process
 
 
 ## alternative text based profiling (done hiddenly in @@profview)
